082606c765b2b6a791cd98bc8e44a85c92776114
thmeme <thierrymeme@gmail.com>
2017-06-08 20:32:43 +0200

    add pages wanted

---
+++ app/js/controllers/wantedController.js
@@ -0,0 +1,63 @@
+angular.module('app')
+  .controller('WantedController', function($scope) {
+    /* Here is your main controller */
+
+
+    $scope.models4 = {
+      selected: null,
+      lists: {
+        A: [{
+            Auteur: 'Jordan',
+            photo: 'jordan.jpg',
+            Competence: "Electricité, peinture",
+            Categorie: 'Bricolage',
+            Commentaire: 'J\'ai vraiment apprécié le moment passé. Une belle rencontre et du beau travail (Martine - Paris).',
+          },
+          {
+            Auteur: 'Nailletine',
+            photo: 'nailletine.jpg',
+            Competence: "Décoration, peinture",
+            Categorie: 'Bricolage',
+            Commentaire: 'Le travail est très soigné (David - La Loupe).',
+          },
+          {
+            Auteur: 'Brigitte',
+            photo: 'brigitte.jpg',
+            Competence: "Plomberie, chauffage",
+            Categorie: 'Bricolage',
+            Commentaire: 'Les bon tuyaux dans les bons trous. Merci Brigitte (Robert - La Ferté-Vidame).',
+          },
+          {
+            Auteur: 'Philippe',
+            photo: 'philippe.jpg',
+            Competence: "Travaux forestiers",
+            Categorie: 'Bricolage',
+            Commentaire: 'Une vraie force de la nature (Pierre - Nogent-le-Rotrou)',
+          }
+        ],
+        B: [{}]
+      }
+    };
+
+    $scope.jardin = true;
+
+    $scope.openJardin = function () {
+      $scope.jardin = false;
+      $scope.bricol = false;
+      $scope.cuisine = false;
+    };
+
+
+    $scope.openBricol = function () {
+      $scope.jardin = true;
+      $scope.bricol = true;
+      $scope.cuisine = false;
+    };
+    $scope.openCuisine = function () {
+      $scope.jardin = true;
+      $scope.bricol = false;
+      $scope.cuisine = true;
+    };
+
+
+  });
